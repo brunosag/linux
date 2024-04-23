6947b9ad5cc9f8b18b76c1b8b4bdd2835b1c73ec
brunosag <brunosag02@gmail.com>
2024-04-23 10:14:22 -0300

    add nitro 5 brightness problem fix

--- ubuntu-script.sh
+++ ubuntu-script.sh
@@ -209,3 +209,11 @@ sudo apt autoremove -y
 
 # Adjust Dock apps and order
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'code_code.desktop']"
+
+# =====================================================================================================================
+#   Fixes
+# =====================================================================================================================
+
+# Fix Nitro 5 brightness problem
+sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash pcie_aspm=force acpi_backlight=native"/' /etc/default/grub
+sudo update-grub
