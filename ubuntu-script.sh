e6c529bc939aaabef42c5a5ad05cfaa4a82aec0d
brunosag <brunosag02@gmail.com>
2024-05-19 21:04:17 -0300

    instead of installing npm, use nvm --lts

--- ubuntu-script.sh
+++ ubuntu-script.sh
@@ -263,5 +263,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 # shellcheck source=/dev/null
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 22
-npm install -g npm@latest
+nvm install node
+nvm use node
+nvm alias default node
